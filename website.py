29e180f3b2cee8956951fbb8f011f42f3393c839
Christian Kim Saporna <qcksaporna@tip.edu.ph>
2026-02-01 22:25:41 +0800

    Update website.py

--- website.py
+++ website.py
@@ -12,7 +12,7 @@ MAX_CLICKS = 14
 # ------------------------------------------------------
 # 📷 PASTE YOUR GIF LINKS HERE
 # ------------------------------------------------------
-BG_GIF = "https://media.giphy.com/media/LpDmM2wSt6Hm5fKJVa/giphy.gif"
+BG_GIF = "https://media.tenor.com/aMOxt0o16TQAAAAi/bubu-bubu-dudu.gif"
 MAIN_GIF = "https://media.tenor.com/wIx44jsp4DMAAAAi/kiss.gif" 
 
 SUCCESS_GIF = "https://media.tenor.com/OrxXcqX25KcAAAAi/dudu-bubu-love-gif.gif"
@@ -182,5 +182,6 @@ if st.session_state.yes_clicks < MAX_CLICKS:
             st.session_state.btn_top = random.randint(10, 80)
             st.session_state.btn_left = random.randint(10, 80)
             st.rerun()
+
 
 
